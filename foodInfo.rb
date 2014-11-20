79c1049a70bfe0bb38e0441567311684cacc5482
Chunxiao Mu <chunxiaomu@seas509.wireless-pennnet.upenn.edu>
2014-11-20 14:06:43 -0500

    gem raw data

--- foodInfo.rb
+++ foodInfo.rb
@@ -1,11 +1,23 @@
 require 'food_info'
+
+require 'fatsecret'
+
+FatSecret.init('1bc2cc311bb24d56a24322272e790419', '42c47bc7053247e2a43adfb8db57e6fc')
+
 FoodInfo.establish_connection(:fat_secret, :key => '1bc2cc311bb24d56a24322272e790419', :secret => '42c47bc7053247e2a43adfb8db57e6fc')
 
+p FatSecret.search_food('milk')
+puts
+p FatSecret.food(800)['food']
+puts
+p FatSecret.search_recipes('chicken',50)
+puts
+p FatSecret.recipe(84411)
 # require 'dalli'
 # client = Dalli::Client.new('localhost:3000')
 # FoodInfo.establish_connection(:fat_secret, :key => '1bc2cc311bb24d56a24322272e790419', :secret => '42c47bc7053247e2a43adfb8db57e6fc', :cache => client)
 
-cheese = FoodInfo.search('cheese steak')
+# cheese = FoodInfo.search('cheese steak')
 
 # print cheese.total_results
 
