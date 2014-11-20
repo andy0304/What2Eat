56c66909689130d58505f7a791b609030588ea3e
Chunxiao Mu <chunxiaomu@hnt.0674.wlanwireless-pennnet.upenn.edu>
2014-11-20 10:10:28 -0500

    try fixing the gem

--- foodInfo.rb
+++ foodInfo.rb
@@ -5,5 +5,26 @@ FoodInfo.establish_connection(:fat_secret, :key => '1bc2cc311bb24d56a24322272e79
 # client = Dalli::Client.new('localhost:3000')
 # FoodInfo.establish_connection(:fat_secret, :key => '1bc2cc311bb24d56a24322272e790419', :secret => '42c47bc7053247e2a43adfb8db57e6fc', :cache => client)
 
-cheese = FoodInfo.search('cheese')
+cheese = FoodInfo.search('cheese steak')
+
 # print cheese.total_results
+
+# def property(sym)
+#   	# do some stuff
+# end
+
+# class SearchResults
+#   include Enumerable
+#   property :results,  :from => :food
+#   property :page,     :from => :page_number
+#   property :per_page, :from => :max_results
+#   property :total_results
+
+#   def initialize(*args)
+    
+#   end      
+  
+# end
+
+# a = SearchResults.new()
+# puts a.class
