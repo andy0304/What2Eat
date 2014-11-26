8e8388accb8b5ba110950d12e22e8d55dcfab063
Chunxiao Mu <tommu180@gmail.com>
2014-11-26 00:56:28 -0500

    Update Gemfile.lock for Heroku

--- Gemfile
+++ Gemfile
@@ -5,7 +5,7 @@ gem 'rails', '3.2.13'
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
-gem 'sqlite3'
+
 
 gem 'twitter-bootstrap-rails'
 gem 'less-rails'
@@ -17,6 +17,12 @@ group :test, :development do
   gem 'database_cleaner'
   gem 'capybara'
   gem 'launchy'
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+  gem 'rails_12factor'
 end
 
 
---
+++ heruku.rb
@@ -0,0 +1,24 @@
+install heruku gives all the keywords
+heruku create # 7500 hours per month per project
+it creates a random url
+setup a git # git -v
+stack is
+
+git push heroku master
+
+gemfile move sqlite to test &development
+gem 'pg'
+
+budle install --without production
+means it does not install production mode but remotely
+
+git commit again to push stuff in the git. 
+
+heroku addons:add heroku postresal
+
+heroku open
+heroku logs
+heroku run rake db:migrate
+
+
+require 'simplecall'
